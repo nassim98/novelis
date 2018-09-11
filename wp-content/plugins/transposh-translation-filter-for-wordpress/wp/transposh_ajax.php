7d27a4989609a2977b5eafdccae5ea6ab8b053d5
IslamB <islamboulila@gmail.com>
2018-09-11 12:03:10 +0100

    single job page

---
+++ wp-content/plugins/transposh-translation-filter-for-wordpress/wp/transposh_ajax.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Transposh v1.0.3
+ * http://transposh.org/
+ *
+ * Copyright 2018, Team Transposh
+ * Licensed under the GPL Version 2 or higher.
+ * http://transposh.org/license
+ *
+ * Date: Sat, 11 Aug 2018 02:00:05 +0300
+ */
+
+/*
+ * This file handles various AJAX needs of our plugin
+ */
+
+echo "This file was removed in 0.8.0 and is only kept for debugging";
+
+?>
--- wp-content/themes/llorix-one-lite/single-job.php
+++ wp-content/themes/llorix-one-lite/single-job.php
@@ -32,42 +32,50 @@ echo 'col-md-8';
 				//get_template_part( 'content', 'job' );
 				echo ' <h2 style="color: #f16c20; margin-top: 10px;
     margin-bottom: 0px; text-align:center; ">';echo get_post_meta(get_the_ID(),'wpcf-poste',true); echo '</h2>';
+
+    echo "<br><br>";
+
+    echo '<img  style="width:500px; margin-left:auto; margin-right:auto;display:block;" src="'. get_post_meta(get_the_ID(),'wpcf-image',true);echo '"></img>';
                 
 
 
-				echo '<h2 style="color: #f16c20; font-size:20px;margin-top: 10px;
-    margin-bottom: 0px;">Description d\'entreprise :</h2>';
+				echo '<br><h2 style="color: #f16c20; font-size:20px;margin-top: 10px;
+    margin-bottom: 0px;">  <i class="fa fa-building colored-text" style="color: #343434;"></i>  Description d\'entreprise :</h2>';
 			//	echo get_post_meta(get_the_ID(),'wpcf-description-d-entreprise',true); 
-				echo "<p style='width:600px;'>".get_post_meta(get_the_ID(),'wpcf-description-d-entreprise',true);echo"</p>";
+				echo "<p style='max-width:600px;'> ".get_post_meta(get_the_ID(),'wpcf-description-d-entreprise',true);echo"</p>";
+
 
 				 /*echo ' <font face="arial" size="4" color="black">Description d\'entreprise </font>';
             echo get_post_meta(get_the_ID(),'wpcf-description-d-entreprise',true); echo "<br>";*/
 
-                  
+                   
 
                 echo ' <h2 style="color: #f16c20; font-size:20px;  margin-top: 10px;
-    margin-bottom: 0px;">Nombres de poste proposés :</h2>';
-                 echo get_post_meta(get_the_ID(),'wpcf-nbr-postes',true);
+    margin-bottom: 0px;">Nombres de poste proposés  : <span style="    color: #313131;">'.get_post_meta(get_the_ID(),'wpcf-nbr-postes',true); echo'</span> </h2>';
+                
+
+
+
 
                   echo ' <h2 style="color: #f16c20; font-size:20px;    margin-top: 10px;
     margin-bottom: 0px;">Description du poste : </h2>';
             echo '<span>'."  ".get_post_meta(get_the_ID(),'wpcf-description-du-poste',true).'</span>';
 
              echo ' <h2 style="color: #f16c20; font-size:20px;    margin-top: 10px;
-    margin-bottom: 0px;">Type de diplôme  :</h2>';
-                echo get_post_meta(get_the_ID(),'wpcf-type-de-diplome',true);echo '<br>';
+    margin-bottom: 0px;"><i class="fa fa-graduation-cap colored-text" style="color: #343434;"></i> Type de diplôme  :</h2>';
+                echo '<p>  '.get_post_meta(get_the_ID(),'wpcf-type-de-diplome',true);echo '</p>';
 
                   echo ' <h2 style="color: #f16c20; font-size:20px;    margin-top: 10px;
-    margin-bottom: 0px;">Niveau d\'experience  : </h2>';
-                 echo get_post_meta(get_the_ID(),'wpcf-niveau-d-experience',true); echo '<br>';
+    margin-bottom: 0px;">Niveau d\'expérience  : </h2>';
+                 echo'<p> '.get_post_meta(get_the_ID(),'wpcf-niveau-d-experience',true); echo '</p>';
 
               echo ' <h2 style="color: #f16c20; font-size:20px;    margin-top: 10px;
-    margin-bottom: 0px;"><i class="fa fa-map-marker colored-text" style="color:grey;"></i> Ville  : </h2>';
+    margin-bottom: 0px;"><i class="fa fa-map-marker colored-text" style="color: #343434;"></i> Ville  : </h2>';
                 echo get_post_meta(get_the_ID(),'wpcf-ville',true); echo '<br>';
 
               echo ' <h2 style="color: #f16c20; font-size:20px;    margin-top: 10px;
     margin-bottom: 0px;">Les langues nécessaires :</h2>';
-                  echo get_post_meta(get_the_ID(),'wpcf-langues',true); echo "<br>";
+                  echo'<p> '. get_post_meta(get_the_ID(),'wpcf-langues',true); echo "</p>";
 
           
 
